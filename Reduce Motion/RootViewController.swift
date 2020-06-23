081c10b0b4e315fb4800e50f610895a3fe7c26d8
Stephen Anthony <steve@darjeelingapps.com>
2020-06-23 19:51:34 +0100

    Add RootViewController

---
+++ Reduce Motion/RootViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RootViewController.swift
+//  Reduce Motion
+//
+//  Created by Stephen Anthony on 20/06/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+/// The view controller responsible for hosting the main scene's user interface.
+final class RootViewController: UIViewController {
+    private let childNavigationController: UINavigationController
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        childNavigationController = UINavigationController(rootViewController: ViewController())
+        childNavigationController.navigationBar.prefersLargeTitles = true
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(childNavigationController)
+        view.addSubview(childNavigationController.view)
+        childNavigationController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            childNavigationController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            childNavigationController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            childNavigationController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            childNavigationController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        childNavigationController.didMove(toParent: self)
+    }
+}
--- Reduce Motion/SceneDelegate.swift
+++ Reduce Motion/SceneDelegate.swift
@@ -14,9 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: ViewController())
-        navigationController.navigationBar.prefersLargeTitles = true
-        window?.rootViewController = navigationController
+        window?.rootViewController = RootViewController()
         window?.makeKeyAndVisible()
     }
 }

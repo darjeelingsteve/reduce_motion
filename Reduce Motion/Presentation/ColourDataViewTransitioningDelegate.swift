eef57acec33eb8eb98e59afca0f490effdf0da6f
Stephen Anthony <steve@darjeelingapps.com>
2020-06-24 19:21:35 +0100

    Add ColourDataViewTransitioningDelegate

---
+++ Reduce Motion/Presentation/ColourDataViewTransitioningDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  ColourDataViewTransitioningDelegate.swift
+//  Reduce Motion
+//
+//  Created by Stephen Anthony on 24/06/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+/// The transitioning delegate to use when presenting a
+/// `ColourDataViewController`.
+final class ColourDataViewTransitioningDelegate: NSObject {
+    private let sourceRect: CGRect
+    
+    init(sourceRect: CGRect) {
+        self.sourceRect = sourceRect
+    }
+}
+
+extension ColourDataViewTransitioningDelegate: UIViewControllerTransitioningDelegate {
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return CentreSquarePresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BouncyTransformTransitionAnimator(style: .present, sourceRect: sourceRect)
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BouncyTransformTransitionAnimator(style: .dismiss, sourceRect: sourceRect)
+    }
+}
